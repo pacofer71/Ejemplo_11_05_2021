4b70526f0a8b0f3167802b1f7d55c3c6f499c1a9
Francisco Fernandez <pacofer71@gmail.com>
2021-05-11 10:50:05 +0200

    rellenando clase

--- src/paquete1/Cuadrado.java
+++ src/paquete1/Cuadrado.java
@@ -1,6 +1,19 @@
 package paquete1;
 
 public class Cuadrado {
+	double lado;
 	
-
+	public Cuadrado() {};
+	public Cuadrado(double miLado) {
+		lado=miLado;
+	}
+	/**
+	 * @return double
+	 */
+	public double calculaPerimetro() {
+		return 4*lado;
+	}
+	public double calcularArea() {
+		return lado*lado;
+	}
 }
--- src/paquete1/Principal.java
+++ src/paquete1/Principal.java
@@ -3,7 +3,8 @@ package paquete1;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Cuadrado miCuadrado=new Cuadrado(5.6);
+		
 
 	}
 
