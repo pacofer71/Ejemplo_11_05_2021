fb6b07c17597a181bd70bf9a2fbf439244a4731f
Francisco Fernandez <pacofer71@gmail.com>
2021-05-11 10:53:55 +0200

    Terminado Pirncipal

--- src/paquete1/Principal.java
+++ src/paquete1/Principal.java
@@ -4,7 +4,8 @@ public class Principal {
 
 	public static void main(String[] args) {
 		Cuadrado miCuadrado=new Cuadrado(5.6);
-		
+		double area=miCuadrado.calcularArea();
+		System.out.println("Area="+area);
 
 	}
 
